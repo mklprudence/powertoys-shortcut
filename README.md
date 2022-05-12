4504aaebea51d457ba4643e55a29a34248a9cb33
mklprudence <mklprudence@gmail.com>
2022-05-12 12:36:42 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Shortcut Plugin for PowerToys Run
+
+A Shortcut Plugin for PowerToys Run. Actions and shortcuts can be customized and used in PowerToys Run
+
+## Installation
+
+**Minimum required version of PowerToys is 0.53.1 - make sure you've got this version installed.**
+
+Extract and copy the content of powertoys-shortcut.zip into `%PROGRAMFILES%\PowerToys\modules\launcher\Plugins` (or wherever your PowerToys is installed), and then restart PowerToys. 
+
+## Usage
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+## References
+This project references [skttl-guid](https://github.com/skttl/ptrun-guid/) as a template due to lack of official documentation on plugins development. 
---
+++ src/powertoys-shortcut/Main.cs
@@ -0,0 +1,71 @@
+﻿using ManagedCommon;
+using Microsoft.PowerToys.Settings.UI.Library;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Wox.Plugin;
+
+namespace powertoys.shortcut
+{
+    public class Main : IPlugin
+    {
+        private string IconPath { get; set; }
+
+        private PluginInitContext Context { get; set; }
+        public string Name => "Shortcut";
+
+        public string Description => "Customized Shortcut and Actions";
+
+        public IEnumerable<PluginAdditionalOption> AdditionalOptions => new List<PluginAdditionalOption>()
+        {
+            new PluginAdditionalOption()
+            {
+                Key = "Test",
+                DisplayLabel = "test",
+                Value = false,
+            },
+        };
+
+        public void Init(PluginInitContext context)
+        {
+            Context = context;
+            Context.API.ThemeChanged += OnThemeChanged;
+            UpdateIconPath(Context.API.GetCurrentTheme());
+        }
+
+        public List<Result> Query(Query query)
+        {
+            var search = query.Search;
+            
+            return new List<Result>
+            {
+                new Result
+                {
+                    Title = search,
+                    IcoPath = IconPath,
+                    Action = e =>
+                    {
+                        Clipboard.SetText(search);
+                        return true;
+                    },
+                }
+            };
+        }
+
+        private void UpdateIconPath(Theme theme)
+        {
+            if (theme == Theme.Light || theme == Theme.HighContrastWhite)
+            {
+                IconPath = "images/guid.light.png";
+            }
+            else
+            {
+                IconPath = "images/guid.dark.png";
+            }
+        }
+
+        private void OnThemeChanged(Theme currentTheme, Theme newTheme)
+        {
+            UpdateIconPath(newTheme);
+        }
+    }
+}
