acc0077ca020326a46f1d5dbbf396464cdea3602
mklprudence <mklprudence@gmail.com>
2022-05-23 16:48:48 +0800

    refactoring Config

---
+++ src/powertoys-shortcut/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace powertoys.shortcut
+{
+    public class Config
+    {
+        public Dictionary<string, Shortcut>? Shortcuts { get; set; }
+    }
+
+    public class Shortcut
+    {
+        public string Action { get; set; }
+    }
+}
--- src/powertoys-shortcut/Main.cs
+++ src/powertoys-shortcut/Main.cs
@@ -131,14 +131,4 @@ namespace powertoys.shortcut
             };
         }
     }
-
-    public class Config
-    {
-        public Dictionary<string, Shortcut>? Shortcuts { get; set; }
-    }
-
-    public class Shortcut
-    {
-        public string Action { get; set; }
-    }
 }
